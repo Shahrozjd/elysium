0e1d8a40f67a48019effe3cff5d329994749d4f7
Abdul Hannan Sabir <abdullhanan709@gmail.com>
2022-07-08 19:48:35 +0500

    update static

--- elysium/settings.py
+++ elysium/settings.py
@@ -129,4 +129,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
